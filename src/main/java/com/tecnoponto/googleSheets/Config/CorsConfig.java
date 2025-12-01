80d6df862765e930892e66a0b1ee0db7ea3bd1f5
Leonardo <leonardosade@gmail.com>
2025-12-01 12:38:23 -0300

    Alteração de domínio

--- src/main/java/com/tecnoponto/googleSheets/Config/CorsConfig.java
+++ src/main/java/com/tecnoponto/googleSheets/Config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500",
-                                "https://frontendleo-iota.vercel.app")
+                                "https://frontendleo-iota.vercel.app", "https://frontendleo-eight.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
